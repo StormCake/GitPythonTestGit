81d09d991b8692b7008409df63c74e6c81673a0e
StormGit <storm47rus@gmail.com>
2020-11-09 01:28:10 +0300

    Done SecondsToDDHHMMSS.py

---
+++ Tests/SecondsToDDHHMMSS.py
@@ -0,0 +1,16 @@
+# Write a Python program to convert seconds to day, hour, minutes and seconds.
+
+seconds = int(input())
+hours = days = 0
+print(f'{seconds} seconds is:')
+
+if seconds:
+    minutes = seconds // 60
+    seconds = seconds % 60
+if minutes:
+    hours = minutes // 60
+    minutes = minutes % 60
+if hours:
+    days = hours // 24
+    hours = hours % 24
+print(f'{days} days, {hours} hours, {minutes} minutes and {seconds} seconds')
