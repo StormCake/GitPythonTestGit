f037f6efc0ab01cd0858218002c4afec8a84d6f8
StormGit <storm47rus@gmail.com>
2020-11-08 21:25:53 +0300

    Done 3sum.py and IsOnFront.py

---
+++ Tests/3sum.py
@@ -0,0 +1,10 @@
+# Write a Python program to calculate the sum of three given numbers,
+# if the values are equal then return three times of their sum.
+a = int(input())
+b = int(input())
+c = int(input())
+if a == b == c:
+    result = 3 * (a+b+c)
+    print(f'Получается да, {result}')
+else:
+    print("не")
---
+++ Tests/IsOnFront.py
@@ -0,0 +1,6 @@
+# Write a Python program to get a new string from a given string where "Is" has been added to the front.
+# If the given string already begins with "Is" then return the string unchanged.
+string = str(input())
+if string[0:2] != 'Is':
+    string = "Is" + string
+print(f'{string}')
